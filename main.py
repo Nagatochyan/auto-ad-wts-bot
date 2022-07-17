c2b1334ae93505dc96713f5eb22edb35d3ce4257
Nagatochyan <94958239+Nagatochyan@users.noreply.github.com>
2022-07-17 15:48:35 +0900

    aa

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ naiyou=input(f'送る内容：')
 t0ken=input(f'貴方のtoken:')
 teisoku=input(f'低速の時間:')
 def qawsed():
-    payload＝{
+    payload={
         'content':naiyou
     }
-header={
+    header={
     'authorization':t0ken
-}
-    r=reqests.post("https://discord.com/api/v9/channels/"+channelid+"/messages",data＝payload,headers=header)
+    }
+    r=reqests.post("https://discord.com/api/v9/channels/"+channelid+"/messages",
+                   data＝payload,headers=header)
 schedule.every(teisoku).minutes.do(qawsed)
