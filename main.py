eee6c8c04691587701bc2ca2bfa989e41022b78c
Nagatochyan <94958239+Nagatochyan@users.noreply.github.com>
2022-07-17 14:12:56 +0900

    test

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+from email.message import _PayloadType
+import requests
+channelid=input(f'サーバーID: ')
+naiyou=input(f'送る内容：')
+def qwerty():
+    payload＝{
+        'content':naiyou
+    }
+    r=reqests.post("https://discord.com/api/v9/channels/"+channelid+"/messages",data＝payload)
